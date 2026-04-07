86429398200212ff70cbaaf7c8207da61d8ecadd
Ivan Alexis Maximoff <maximoff.ivan@gmail.com>
2026-04-07 19:30:30 -0300

    refactor: unify common RabbitMQ logic into a private base class

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -3,17 +3,13 @@ import random
 import string
 from .middleware import MessageMiddlewareCloseError, MessageMiddlewareDisconnectedError, MessageMiddlewareMessageError, MessageMiddlewareQueue, MessageMiddlewareExchange
 
-class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
-
-    def __init__(self, host, queue_name):
-        self.queue_name = queue_name
+class _RabbitMQMiddleware:
+    def __init__(self, host):
         try:
             self._connection = pika.BlockingConnection(
                 pika.ConnectionParameters(host=host)
             )
             self._channel = self._connection.channel()
-            self._channel.queue_declare(queue=self.queue_name)
-
         except pika.exceptions.AMQPConnectionError:
             print(f"Error conectando a RabbitMQ en {host}")
             raise
@@ -25,87 +21,10 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         except Exception:
             raise MessageMiddlewareCloseError()
 
-    def send(self, message):
+    def _publish(self, exchange, routing_key, message):
         try:
             self._channel.basic_publish(
-                exchange='',
-                routing_key=self.queue_name,
-                body=message
-            )
-        except pika.exceptions.AMQPConnectionError:
-            raise MessageMiddlewareDisconnectedError()
-        except Exception:
-            raise MessageMiddlewareMessageError()
-
-    def start_consuming(self, on_message_callback):
-        def internal_callback(ch, method, _, body):
-            def ack():
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-            def nack():
-                ch.basic_nack(delivery_tag=method.delivery_tag)
-            on_message_callback(body, ack, nack)
-
-        try:
-            self._channel.basic_consume(
-                queue=self.queue_name,
-                on_message_callback=internal_callback
-            )
-            self._channel.start_consuming()
-        except pika.exceptions.AMQPConnectionError:
-            raise MessageMiddlewareDisconnectedError()
-        except pika.exceptions.ConnectionClosedByBroker:
-            pass
-
-    def stop_consuming(self):
-        try:
-            if self._channel and self._channel.is_open:
-                self._channel.stop_consuming()
-        except Exception:
-            pass
-
-class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    
-    def __init__(self, host, exchange_name, routing_keys):
-        self.exchange_name = exchange_name
-        self.routing_keys = routing_keys
-
-        try:
-            self._connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=host)
-            )
-            self._channel = self._connection.channel()
-
-            self._channel.exchange_declare(
-                exchange=self.exchange_name,
-                exchange_type='direct'
-            )
-
-            result = self._channel.queue_declare(queue='', exclusive=True)
-            random_queue_name = result.method.queue
-            self.queue_name = random_queue_name
-
-            for routing_key in self.routing_keys:
-                self._channel.queue_bind(
-                    exchange=self.exchange_name,
-                    queue=self.queue_name,
-                    routing_key=routing_key
-                )
-        except Exception:
-            raise
-
-    def close(self):
-        try:
-            if self._connection.is_open:
-                self._connection.close()
-        except Exception:
-            raise MessageMiddlewareCloseError()
-
-    def send(self, message):
-        try:
-            routing_key = self.routing_keys[0] if self.routing_keys else ''
-            
-            self._channel.basic_publish(
-                exchange=self.exchange_name,
+                exchange=exchange,
                 routing_key=routing_key,
                 body=message
             )
@@ -139,3 +58,38 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
                 self._channel.stop_consuming()
         except Exception:
             pass
+
+class MessageMiddlewareQueueRabbitMQ(_RabbitMQMiddleware, MessageMiddlewareQueue):
+    def __init__(self, host, queue_name):
+        _RabbitMQMiddleware.__init__(self, host)
+        self.queue_name = queue_name
+        self._channel.queue_declare(queue=self.queue_name)
+
+    def send(self, message):
+        self._publish(exchange='', routing_key=self.queue_name, message=message)
+
+class MessageMiddlewareExchangeRabbitMQ(_RabbitMQMiddleware, MessageMiddlewareExchange):
+    def __init__(self, host, exchange_name, routing_keys):
+        _RabbitMQMiddleware.__init__(self, host)
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+
+        self._channel.exchange_declare(
+            exchange=self.exchange_name,
+            exchange_type='direct'
+        )
+
+        result = self._channel.queue_declare(queue='', exclusive=True)
+        random_queue_name = result.method.queue
+        self.queue_name = random_queue_name
+
+        for routing_key in self.routing_keys:
+            self._channel.queue_bind(
+                exchange=self.exchange_name,
+                queue=self.queue_name,
+                routing_key=routing_key
+            )
+
+    def send(self, message):
+        rk = self.routing_keys[0] if self.routing_keys else ''
+        self._publish(exchange=self.exchange_name, routing_key=rk, message=message)
